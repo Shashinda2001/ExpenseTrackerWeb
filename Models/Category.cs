385a31a475fd58ad39ccaa284f6b2ff9417308a8
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-12-23 12:12:49 +0530

    Add NotMapped property TitleWithIcon to concatenate Icon and Title

--- Models/Category.cs
+++ Models/Category.cs
@@ -17,5 +17,10 @@ namespace ExpenseTrackerWeb.Models
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } ="Expense";
 
+        [NotMapped]
+        public string? TitleWithIcon { get { 
+            return this.Icon + " " + this.Title;
+            } }
+
     }
 }
