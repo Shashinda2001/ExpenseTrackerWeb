9cf250f4af94f0c04da7b6f7f4eb9bacfdb07b1f
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2025-01-08 16:49:19 +0530

    Category can nullable

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -9,7 +9,7 @@ namespace ExpenseTrackerWeb.Models
         public int TransactionId { get; set; }
 
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
 
         public int Amount { get; set; }
 
